2311f391130c3af11c327f7af064e7c4f5919c5d
Artem <artem_kazakovtsev@mail.ru>
2020-01-29 18:04:40 +0500

    Snake

---
+++ labirint/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace labirint
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.timer3 = new System.Windows.Forms.Timer(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.button1.Location = new System.Drawing.Point(204, 315);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Button1_KeyDown);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.label2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.label2.Location = new System.Drawing.Point(496, 46);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(0, 13);
+            this.label2.TabIndex = 6;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.label1.Location = new System.Drawing.Point(23, 50);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(58, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Your score";
+            this.label1.Visible = false;
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 5000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.label4.Location = new System.Drawing.Point(440, 37);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 13);
+            this.label4.TabIndex = 13;
+            this.label4.Visible = false;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.pictureBox1.Location = new System.Drawing.Point(120, 162);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(250, 152);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 15;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Visible = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.ImeMode = System.Windows.Forms.ImeMode.NoControl;
+            this.pictureBox2.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(100, 100);
+            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox2.TabIndex = 16;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Visible = false;
+            // 
+            // timer3
+            // 
+            this.timer3.Enabled = true;
+            this.timer3.Interval = 10;
+            this.timer3.Tick += new System.EventHandler(this.timer3_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(498, 499);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Labirint";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Timer timer3;
+    }
+}
+
---
+++ labirint/Form1.cs
@@ -0,0 +1,331 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace labirint
+{
+    public partial class Form1 : Form
+    {
+        Image head = Image.FromFile(@"RES\head.png");
+        Image grass = Image.FromFile(@"RES\grass.png");
+        Image apple = Image.FromFile(@"RES\apple.png");
+        Image tail = Image.FromFile(@"RES\tail.png");
+
+        Bitmap bitmap = new Bitmap(100, 100);
+
+        Pen BlackPen = new Pen(Color.Black);
+        Pen GrayPen = new Pen(Color.Gray);
+        Pen GreenPen = new Pen(Color.Green);
+        Pen RedPen = new Pen(Color.Red);
+        Pen SeaGreenPen = new Pen(Color.SeaGreen);
+
+        Brush BlackBrush = new SolidBrush(Color.Black);
+        Brush GrayBrush = new SolidBrush(Color.Gray);
+        Brush GreenBrush = new SolidBrush(Color.Green);
+        Brush RedBrush = new SolidBrush(Color.Red);
+        Brush SeaGreenBrush = new SolidBrush(Color.SeaGreen);
+
+        Random random = new Random();
+
+        System.Media.SoundPlayer UDIED = new System.Media.SoundPlayer(@"RES\DED.wav");
+        System.Media.SoundPlayer SIZEPlUS = new System.Media.SoundPlayer(@"RES\LVLUP.wav");
+
+        int[,] map = new int[10, 10];
+        int[] where = new int[2];
+        int RX, RY, size, razmer, howRender, mashtab = 0;
+        string move;
+        bool can = false;
+
+        Queue<int> partX = new Queue<int>();
+
+        Queue<int> partY = new Queue<int>();
+
+
+        void NextHwost()
+        {
+            //score
+            label1.Text = "Your score" + "\n" + Convert.ToString(size);
+
+            //clear
+            for (int i = 0; i < razmer; i++)
+            {
+                for (int j = 0; j < razmer; j++)
+                {
+                    if (map[i, j] == 1)
+                    {
+                        map[i, j] = 0;
+                    }
+                }
+            }
+
+            //queue
+            partX.Enqueue(where[0]);
+            partY.Enqueue(where[1]);
+            if (howRender > size)
+            {
+                partX.Dequeue();
+                partY.Dequeue();
+                howRender--;
+            }
+            howRender++;
+
+            //addtomap
+            for (int x = 0; x < partX.Count(); x++)
+            {
+                map[partX.ToArray()[x], partY.ToArray()[x]] = 1;
+            }
+        }
+
+        void Uupdate()
+        {
+            Graphics graphics = Graphics.FromImage(bitmap);
+
+            graphics.Clear(Color.White);
+
+            graphics.DrawRectangle(BlackPen, 0, 0, razmer * mashtab, razmer * mashtab);
+            graphics.FillRectangle(BlackBrush, 0, 0, razmer * mashtab, razmer * mashtab);
+
+            for (int x = 0; x < razmer * mashtab; x += 1 * mashtab)
+            {
+                for (int z = 0; z < razmer * mashtab; z += 1 * mashtab)
+                {
+                    switch (map[z / mashtab, x / mashtab])
+                    {
+                        case 0:
+                            graphics.DrawRectangle(GrayPen, x, z, mashtab, mashtab);
+                            graphics.FillRectangle(GrayBrush, x, z, mashtab, mashtab);
+                            graphics.DrawImage(grass, x, z, mashtab, mashtab);
+                            break;
+                        case 1:
+                            graphics.DrawRectangle(GreenPen, x, z, mashtab, mashtab);
+                            graphics.FillRectangle(GreenBrush, x, z, mashtab, mashtab);
+                            graphics.DrawImage(tail, x, z, mashtab, mashtab);
+                            break;
+                        case 2:
+                            graphics.DrawRectangle(RedPen, x, z, mashtab, mashtab);
+                            graphics.FillRectangle(RedBrush, x, z, mashtab, mashtab);
+                            graphics.DrawImage(apple, x, z, mashtab, mashtab);
+                            break;
+                    }
+                }
+
+            }
+
+            graphics.DrawImage(head, where[1] * mashtab, where[0] * mashtab, mashtab, mashtab);
+
+            pictureBox2.Image = bitmap;
+        }
+
+        void toomuch()
+        {
+
+            partX.Clear();
+            partY.Clear();
+
+            this.BackColor = Color.FromArgb(0, 0, 0);
+            pictureBox1.Visible = true;
+            UDIED.Play();
+            label2.Visible = false;
+            timer1.Enabled = false;
+            timer2.Enabled = false;
+            label1.Visible = false;
+            button1.Enabled = true;
+            pictureBox2.Visible = false;
+        }
+
+        void goLeft()
+        {
+            if (where[1] == 0 || map[where[0], where[1] - 1] == 1)
+            {
+                toomuch();
+            }
+            else
+            {
+                if (map[where[0], where[1] - 1] == 2)
+                {
+                    size++;
+                    SIZEPlUS.Play();
+                }
+                map[where[0], where[1]] = 0;
+                map[where[0], where[1] - 1] = 1;
+                where[1] -= 1;
+            }
+            NextHwost();
+        }
+
+        void goUp()
+        {
+            if (where[0] == 0 || map[where[0] - 1, where[1]] == 1)
+            {
+                toomuch();
+            }
+            else
+            {
+                if (map[where[0] - 1, where[1]] == 2)
+                {
+                    size++;
+                    SIZEPlUS.Play();
+                }
+                map[where[0], where[1]] = 0;
+                map[where[0] - 1, where[1]] = 1;
+                where[0] -= 1;
+            }
+            NextHwost();
+        }
+
+        void goDown()
+        {
+            if (where[0] == razmer - 1 || map[where[0] + 1, where[1]] == 1)
+            {
+                toomuch();
+            }
+            else
+            {
+                if (map[where[0] + 1, where[1]] == 2)
+                {
+                    size++;
+                    SIZEPlUS.Play();
+                }
+                map[where[0], where[1]] = 0;
+                map[where[0] + 1, where[1]] = 1;
+                where[0] += 1;
+            }
+            NextHwost();
+        }
+
+        void goRight()
+        {
+            if (where[1] == razmer - 1 || map[where[0], where[1] + 1] == 1)
+            {
+                toomuch();
+            }
+            else
+            {
+                if (map[where[0], where[1] + 1] == 2)
+                {
+                    size++;
+                    SIZEPlUS.Play();
+                }
+                map[where[0], where[1]] = 0;
+                map[where[0], where[1] + 1] = 1;
+                where[1] += 1;
+            }
+            NextHwost();
+        }
+
+        public void Razmer()
+        {
+            razmer = 10;
+            map = new int[razmer, razmer];
+
+            bitmap = new Bitmap(razmer * mashtab, razmer * mashtab);
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            partX = new Queue<int>();
+            partY = new Queue<int>();
+            map = new int[10, 10];
+            mashtab = 50;
+            Razmer();
+
+            size = 0;
+            howRender = 0;
+
+            pictureBox2.Visible = true;
+            label2.Visible = true;
+            timer1.Enabled = true;
+            timer2.Enabled = true;
+            pictureBox1.Visible = false;
+
+            this.BackColor = Color.FromArgb(255, 255, 255);
+            move = "right";
+            label1.Visible = true;
+
+            map[0, 0] = 1;
+            where[0] = 0;
+            where[1] = 0;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Interval = 1000 - (size * 10);
+            timer2.Interval = 5000 - (size * 10);
+            switch (move)
+            {
+                case "up":
+                    goUp();
+                    break;
+                case "down":
+                    goDown();
+                    break;
+                case "right":
+                    goRight();
+                    break;
+                case "left":
+                    goLeft();
+                    break;
+            }
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+            can = true;
+            while (can)
+            {
+                RX = random.Next(0, razmer);
+                RY = random.Next(0, razmer);
+                if (map[RX, RY] == 0)
+                {
+                    can = false;
+                }
+                else
+                {
+                    can = true;
+                }
+            }
+            can = true;
+            map[RX, RY] = 2;
+        }
+        private void Button1_KeyDown(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyValue)
+            {
+                case (char)Keys.W:
+                    move = "up";
+                    break;
+
+                case (char)Keys.D:
+                    move = "right";
+                    break;
+
+                case (char)Keys.A:
+                    move = "left";
+                    break;
+
+                case (char)Keys.S:
+                    move = "down";
+                    break;
+
+                default:
+                    break;
+            }
+        }
+
+        private void timer3_Tick(object sender, EventArgs e)
+        {
+            Uupdate();
+        }
+    }
+}
